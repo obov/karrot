2e2e67f5a4346328e705c9ef0c856143c315d3e8
obov <obov3e@gmail.com>
2022-05-19 14:28:46 +0900

    add config

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    reactRoot: true,
+    // runtime: "nodejs",
+    // serverComponents: true,
+  },
+  images: {
+    domains: ["imagedelivery.net", "videodelivery.net"],
+  },
+};
+
+module.exports = nextConfig;
