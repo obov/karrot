6b0e9eb8534ff95280f607db91b7c25379e65896
obov <obov3e@gmail.com>
2022-05-19 14:19:25 +0900

    done except challenge

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -20,33 +20,29 @@ interface ReviewResponse {
 
 const Profile: NextPage = () => {
   const { user } = useUser();
-  console.log("user : ", user);
   const { data } = useSWR<ReviewResponse>("http://localhost:3000/api/reviews");
-  console.log("data : ", data);
   return (
     <Layout title="profile" hasTabBar seoTitle="Profile">
       <div className="py-10 px-4">
         <div className="flex items-center space-x-3">
-          <Suspense fallback={<span>loading...</span>}>
-            {user?.avatar ? (
-              <Image
-                width={64}
-                height={64}
-                src={`https://imagedelivery.net/QiWuyrvCeOnHYrH0LRUbDg/${user?.avatar}/avatar`}
-                className="w-16 h-16 bg-slate-500 rounded-full -z-10"
-                alt=""
-              />
-            ) : (
-              <div className="w-16 h-16 bg-slate-500 rounded-full" />
-            )}
+          {user?.avatar ? (
+            <Image
+              width={64}
+              height={64}
+              src={`https://imagedelivery.net/QiWuyrvCeOnHYrH0LRUbDg/${user?.avatar}/avatar`}
+              className="w-16 h-16 bg-slate-500 rounded-full -z-10"
+              alt=""
+            />
+          ) : (
+            <div className="w-16 h-16 bg-slate-500 rounded-full" />
+          )}
 
-            <div className="flex flex-col ">
-              <span className="font-medium text-gray-900">{user?.name}</span>
-              <Link href="/profile/edit">
-                <a className="text-sm text-gray-700">Edit profile &rarr;</a>
-              </Link>
-            </div>
-          </Suspense>
+          <div className="flex flex-col ">
+            <span className="font-medium text-gray-900">{user?.name}</span>
+            <Link href="/profile/edit">
+              <a className="text-sm text-gray-700">Edit profile &rarr;</a>
+            </Link>
+          </div>
         </div>
         <div className="mt-10 flex justify-around">
           <Link href="/profile/sold">
@@ -119,69 +115,64 @@ const Profile: NextPage = () => {
             </a>
           </Link>
         </div>
-        <Suspense fallback={<span>loading...</span>}>
-          {data?.reviews?.map((review) => (
-            <div key={review.id} className="mt-12">
-              <div className="flex space-x-4 items-center">
-                <div className="w-12 h-12 rounded-full bg-slate-600" />
-                <div>
-                  <h4 className="text-sm font-bold text-gray-900">
-                    {review.writer.name}
-                  </h4>
-                  <div className="flex items-center">
-                    {[1, 2, 3, 4, 5].map((star) => (
-                      <svg
-                        key={star}
-                        className={cls(
-                          "h-5 w-5",
-                          review.score >= star
-                            ? "text-yellow-400 "
-                            : "text-gray-400 "
-                        )}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                      </svg>
-                    ))}
-                  </div>
+        {data?.reviews?.map((review) => (
+          <div key={review.id} className="mt-12">
+            <div className="flex space-x-4 items-center">
+              <div className="w-12 h-12 rounded-full bg-slate-600" />
+              <div>
+                <h4 className="text-sm font-bold text-gray-900">
+                  {review.writer.name}
+                </h4>
+                <div className="flex items-center">
+                  {[1, 2, 3, 4, 5].map((star) => (
+                    <svg
+                      key={star}
+                      className={cls(
+                        "h-5 w-5",
+                        review.score >= star
+                          ? "text-yellow-400 "
+                          : "text-gray-400 "
+                      )}
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                  ))}
                 </div>
               </div>
-              <div className="mt-4 text-gray-600 text-sm">
-                <p>{review.review}</p>
-              </div>
             </div>
-          ))}
-        </Suspense>
+            <div className="mt-4 text-gray-600 text-sm">
+              <p>{review.review}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </Layout>
   );
 };
-const Page: NextPage = () =>
-  // {
-  //<{ profile: User; reviews: ReviewWithUser[] }>
-  // profile,
-  // reviews,
-  // }
-  {
-    return (
-      <SWRConfig
-        value={{
-          suspense: true,
-          // fallback: {
-          //   "/api/users/me": { ok: true, profile },
-          //   "/api/reviews": { ok: true, reviews },
-          // },
-        }}
-      >
-        <Profile />
-      </SWRConfig>
-    );
-  };
+const Page: NextPage<{ profile: User; reviews: ReviewWithUser[] }> = ({
+  profile,
+  reviews,
+}) => {
+  return (
+    <SWRConfig
+      value={{
+        // suspense: true,
+        fallback: {
+          "/api/users/me": { ok: true, profile },
+          "/api/reviews": { ok: true, reviews },
+        },
+      }}
+    >
+      <Profile />
+    </SWRConfig>
+  );
+};
 
-/* export const getServerSideProps = withSsrSession(
+export const getServerSideProps = withSsrSession(
   async ({ req }: NextPageContext) => {
     const profile = jsonSP(
       await client.user.findUnique({
@@ -209,5 +200,5 @@ const Page: NextPage = () =>
       props: { profile, reviews },
     };
   }
-); */
+);
 export default Page;
