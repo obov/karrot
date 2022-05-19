c8875b199327199f6fff28a5732b57a10cd85c47
obov <obov3e@gmail.com>
2022-05-19 14:42:47 +0900

    changed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    reactRoot: true,
-    // runtime: "nodejs",
-    // serverComponents: true,
-  },
   images: {
     domains: ["imagedelivery.net", "videodelivery.net"],
   },
--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,10 +4,10 @@ interface Cookies {}
 const middleware = (req: NextRequest, evt: NextFetchEvent) => {
   if (req?.ua?.isBot) {
     return new Response("Not bot plz", { status: 403 });
-  } else if (req?.ua?.isBot === undefined) {
+  } else if (req?.ua?.isBot !== undefined) {
     // unstable_revalidate
     if (!req?.url.includes("/api")) {
-      if (!req?.url.includes("/enter") && !req.cookies.get("karrot")) {
+      if (!req?.url.includes("/enter") && !req.cookies.karrot) {
         const url = req.nextUrl.clone();
         url.pathname = "/enter";
         return NextResponse.redirect(url);
